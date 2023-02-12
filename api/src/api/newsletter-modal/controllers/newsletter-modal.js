be7fdf35089e6a4bd93e01c1de39f3fcd1c656fb
OtsoLap <59653348+otsolap@users.noreply.github.com>
2023-02-12 19:39:55 +0000

    newsletter modal done for site

---
+++ api/src/api/newsletter-modal/controllers/newsletter-modal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newsletter-modal controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::newsletter-modal.newsletter-modal');
---
+++ api/src/api/newsletter-modal/routes/newsletter-modal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newsletter-modal router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::newsletter-modal.newsletter-modal');
