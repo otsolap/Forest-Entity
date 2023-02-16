d73b62e5ea30783166d29b60d5847e36d127d304
OtsoLap <59653348+otsolap@users.noreply.github.com>
2023-02-16 19:04:18 +0000

    putting up boilerplate code

---
+++ client/components/Layout.js
@@ -0,0 +1,9 @@
+export default function Layout({ children }) {
+    return (
+        <div className="layout">
+            <main className="content">
+                {children}
+            </main>
+        </div>
+    )
+}
