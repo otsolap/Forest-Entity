8b5a57e714134fbadb2f7a255d598f2e3d125fa4
OtsoLap <59653348+otsolap@users.noreply.github.com>
2023-02-18 16:33:01 +0000

    deleted beta nextjs feat and worked on 2 components

---
+++ client/components/partials/Image.js
@@ -0,0 +1,20 @@
+import { getStrapiMedia } from "util/media";
+import Image from "next/image";
+
+const Image = ({ image, width, height, sizes }) => {
+  const { alt } = image.data.attributes;
+
+  return (
+    <Image
+      src={getStrapiMedia(image)}
+      alt={alt || ""}
+      quality={100}
+      width={width}
+      height={height}
+      priority
+      sizes={sizes}
+    />
+  );
+};
+
+export default Image;
