c4c50ef911bef6d3a28eefd55a727c6181153437
OtsoLap <59653348+otsolap@users.noreply.github.com>
2023-02-19 16:24:33 +0000

    renamed folder

--- client/components/Meta.js
+++ client/components/Meta.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useContext } from 'react'
 import { GlobalContext } from 'pages/_app'
-import { getStrapiMedia } from 'util/media'
+import { getStrapiMedia } from 'utils/media'
 
 const Meta = ({ meta }) => (
     <Head>
