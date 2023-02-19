bf00577a3cfc9c15fd277307242c09b513872c5f
OtsoLap <59653348+otsolap@users.noreply.github.com>
2023-02-19 12:44:03 +0000

    wip on blocks and working on frontend api

--- client/util/api.js
+++ client/util/api.js
@@ -1,120 +1,199 @@
-import { URLSearchParams } from "next/dist/compiled/@edge-runtime/primitives/url"
+import { URLSearchParams } from "next/dist/compiled/@edge-runtime/primitives/url";
 
 export function getStrapiURL(path) {
-    return `${
-        process.env.STRAPI_PUBLIC_API_URL || 'http:localhost:1337'
-    }$path`
+  return `${process.env.STRAPI_PUBLIC_API_URL || "http:localhost:1337"}$path`;
 }
 
 /**
- * 
+ *
  * @param {string} path Path of the API
- * @param {object} path 
+ * @param {object} path
  * @param {requestInit} options Options passed to fetch
- * @returns Parsed API call resonse 
+ * @returns Parsed API call resonse
  */
 export async function fetchAPI(path, urlParamsObject = {}, options = {}) {
-    // Merge default and user options
-    const mergedOptions = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        ...options,
-    }
+  // Merge default and user options
+  const mergedOptions = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    ...options,
+  };
 
-    // Build the request URL
-    const queryString = new URLSearchParams(urlParamsObject).toString();
-    const requestUrl = `${getStrapiURL(
-        `/api${path}${queryString ? `?${queryString}` : ""}`
-      )}`
-    
-      // trigger API call
-      const response = await fetch(requestUrl, mergedOptions)
+  // Build the request URL
+  const queryString = new URLSearchParams(urlParamsObject).toString();
+  const requestUrl = `${getStrapiURL(
+    `/api${path}${queryString ? `?${queryString}` : ""}`
+  )}`;
 
-    // handle response
-    if(!response.ok) {
-        console.error(response.statusText)
-        throw new Error(`An error occured please try again`)
-    }
-    const data = await response.json()
-    return data
+  // trigger API call
+  const response = await fetch(requestUrl, mergedOptions);
+
+  // handle response
+  if (!response.ok) {
+    console.error(response.statusText);
+    throw new Error(`An error occured please try again`);
+  }
+  const data = await response.json();
+  return data;
 }
 
 /**
- * 
+ *
  * @param {Object} options
  * @param {string} options.slug the page's slug
  */
 
-export async function getPageData({ slug}) {
-    // find pages that match this slug
-    const gqEndpoint = getStrapiURL('/graphql')
-    const pageRes = await fetch(gqEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            query: `
-            page {
-              data {
-                attributes {
-                  title
-                  slug
-                  seo {
-                    title
-                    description
-                    meta {
+export async function getPageData({ slug }) {
+  // find pages that match this slug
+  const gqEndpoint = getStrapiURL("/graphql");
+  const pageRes = await fetch(gqEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+      query GetPages(
+        $slug: String!
+        $publicationState: PublicationState!
+        $locale: I18NLocaleCode!
+      ){
+      pages {
+        data {
+          attributes {
+            title
+            filters: { slug: { eq: $slug } }
+            seo {
+              title
+              description
+              meta {
+                title
+                description
+              }
+              preventIndexing
+              structuredData
+            }
+            blocks {
+              __typename
+              ... on ComponentBlocksAccordion {
+                title
+                description
+                faqs {
+                  data {
+                    attributes {
                       title
-                      description
+                      body
                     }
-                    preventIndexing
-                    structuredData
                   }
-                  blocks {
-                    __typename
-                    ... on ComponentBlocksAccordion {
+                }
+              }
+              ... on ComponentBlocksBlogs {
+                title
+                description
+                filter
+                articles {
+                  data {
+                    attributes {
                       title
                       description
-                      faqs {
+                      publishedAt
+                      category {
                         data {
                           attributes {
                             title
-                            body
                           }
                         }
                       }
                     }
                   }
                 }
+                link {
+                  href
+                  title
+                  isExternal
+                  target
+                }
+              }
+              ... on ComponentBlocksCards {
+                title
+                description
+                Card {
+                  title
+                  description
+                }
+              }
+              ... on ComponentBlocksHero {
+                title
+                description
+                alignContent
+                mediaWidth
+                video
+                autoplay
+                media
+                Image {
+                  alt
+                }
+                buttons {
+                  ahref
+                  title
+                  isExternal
+                  target
+                  selectTheme
+                }
+              }
+              ... on ComponentBlocksHighlight {
+                title
+                description
+                button {
+                  ahref
+                  title
+                  isExternal
+                  target
+                  selectTheme
+                }
+                selectTheme
+              }
+              ... on ComponentBlocksMediaMix {
+                item {
+                  type
+                  markdown
+                  video
+                }
+              }
+              ... on ComponentBlocksTextArea {
+                text
               }
             }
-            `
-        })
-    })
+          }
+        }
+      }
+      `,
+    }),
+  });
 
-    const pageData = await pageRes.json()
+  const pageData = await pageRes.json();
 
-    // null identifier
-    if(pageData.data?.page == null || pagesData.data.page.slength === 0) {
-        return null
-    }
+  // null identifier
+  if (pageData.data?.page == null || pagesData.data.page.slength === 0) {
+    return null;
+  }
 
-    // always return the first item, since there should be one per page only.
-    return pagesData.data.pages.data[0]
+  // always return the first item, since there should be one per page only.
+  return pagesData.data.pages.data[0];
 }
 
 // Site data (Meta, Navigation, global settings)
 export async function getGlobalData() {
-    // find pages that match this slug
-    const gqEndpoint = getStrapiURL('/graphql')
-    const globalRes = await fetch(gqEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            query: `
+  // find pages that match this slug
+  const gqEndpoint = getStrapiURL("/graphql");
+  const globalRes = await fetch(gqEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
             navigation {
               data {
                 attributes {
@@ -175,10 +254,10 @@ export async function getGlobalData() {
                   }
                 }
               }
-            `
-        })
-    })
+            `,
+    }),
+  });
 
-    const global = await globalRes.json()
-    return global.data.global
+  const global = await globalRes.json();
+  return global.data.global;
 }
